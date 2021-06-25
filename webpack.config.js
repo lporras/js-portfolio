9753b3345538705c4792344cd117d59a7f319c27
Luis Alfredo Porras Paez <luis@webdox.cl>
2021-06-25 19:10:18 -0400

    store images in assets

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    assetModuleFilename: "assets/images/[hash]"
   },
   resolve: {
     extensions: ['.js']
