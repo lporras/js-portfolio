a4c4afe53aad8027e1f2724adf47db6618862a00
Luis Alfredo Porras Paez <luis@webdox.cl>
2021-06-26 12:36:15 -0400

    minimize css and js, and using content hash

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin =  require('html-webpack-plugin');
 const MiniCssExtractPlugin  =  require('mini-css-extract-plugin');
 //const CopyPlugin  =  require('copy-webpack-plugin');
+const CssMinimizerPlugin  = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     assetModuleFilename: "assets/images/[hash][ext][query]"
   },
   resolve: {
@@ -41,7 +43,7 @@ module.exports = {
           options: {
             limit: 10000,
             mimeType: "application/font-woff",
-            name: "[name].[ext]",
+            name: "[name].[contenthash].[ext]",
             outputPath: "./assets/fonts/",
             publicPath: "./assets/fonts/",
             esModule: false
@@ -56,7 +58,9 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html'
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    })
     /*new CopyPlugin({
       patterns: [
         {
@@ -65,5 +69,12 @@ module.exports = {
         }
       ]
     })*/
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
 }
