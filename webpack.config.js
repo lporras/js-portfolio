ca2aa402ad45daa1f76127cf15747c46a4186dd4
Luis Alfredo Porras Paez <luis@webdox.cl>
2021-06-26 12:22:17 -0400

    load fonts

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    assetModuleFilename: "assets/images/[hash]"
+    assetModuleFilename: "assets/images/[hash][ext][query]"
   },
   resolve: {
     extensions: ['.js']
@@ -33,6 +33,20 @@ module.exports = {
       {
         test: /\.png$/,
         type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimeType: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false
+          }
+        }
       }
     ]
   },
