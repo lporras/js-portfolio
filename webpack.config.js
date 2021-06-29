4cdfbc19709279cb6f3f7983dd894b3a71d5a62f
Luis Alfredo Porras Paez <luis@webdox.cl>
2021-06-29 19:01:02 -0400

    using API ENV variable

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin  =  require('mini-css-extract-plugin');
 //const CopyPlugin  =  require('copy-webpack-plugin');
 const CssMinimizerPlugin  = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -66,7 +67,8 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'assets/[name].[contenthash].css'
-    })
+    }),
+    new Dotenv(),
     /*new CopyPlugin({
       patterns: [
         {
